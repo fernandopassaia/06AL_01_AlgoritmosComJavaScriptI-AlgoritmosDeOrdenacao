9daf2fe3acef0135c26e621787d5218bd37a31dc
FernandoPassaia MacBook Pro M4 <fernandopassaia@futuradata.com.br>
2025-07-16 21:42:35 -0300

    InsertionSort Algorithm is done - now 4 module of the course

--- Parte03_InsertionSort_Fernando/oleosMotorInsertionSort.js
+++ Parte03_InsertionSort_Fernando/oleosMotorInsertionSort.js
@@ -6,13 +6,15 @@ function insertionSort(oleosMotor){
         // atual maior do que zero - assim quando estiver no indice 0 ele não vai tentar -1
         // vai comparar se o preco do livro ATUAL é menor que o do livro ANTERIOR
         // se preco do livro atual for MENOR que livro anterior, ele vai ENTRAR e trocar eles de lugar
-        while(atual > 0 && oleosMotor[atual].preco < oleosMotor[atual - 1].preco){
+        while(atual > 0 && oleosMotor[atual].valor < oleosMotor[atual - 1].valor){
             let oleoAtual = oleosMotor[atual];
             let oleoAnterior = oleosMotor[atual - 1];
 
             oleosMotor[atual] = oleoAnterior;
             oleosMotor[atual - 1] = oleoAtual;
 
+            // enquanto o item atual tiver valor MENOR que o item anterior, eu vou decrementando
+            // o indice e VOLTANDO no array até achar a posição correta
             atual--;
 }
     }
